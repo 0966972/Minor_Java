9785c65f8277934c083486f2c216aa77ca26aba5
Maurice Pakvis <0966972@hr.nl>
2020-10-08 21:19:06 +0200

    verkeerde branch

--- demo/src/main/java/com/example/demo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import com.example.entity.UserEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
-}
